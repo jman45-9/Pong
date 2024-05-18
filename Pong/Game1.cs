61763433b68ead0b124f7fcef6e63a3ef5c012ce
Jeremy Cope <jcope0787@protonmail.com>
2024-05-17 23:10:29 -0700

    Add project files.

---
+++ Pong/Game1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Paddle _p1Paddle;
+        private Paddle _p2Paddle;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            int wallDis = 30;
+            _p1Paddle = new Paddle(null, new Vector2(wallDis, _graphics.PreferredBackBufferWidth / 2));
+            _p2Paddle = new Paddle(null, new Vector2(_graphics.PreferredBackBufferWidth - wallDis, _graphics.PreferredBackBufferWidth / 2));
+            
+            
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            var paddleTex = Content.Load<Texture2D>("paddleSprite");
+            _p1Paddle.Texture = paddleTex;
+            _p2Paddle.Texture = paddleTex;
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.W)) {
+                _p1Paddle.moveUp();
+            } else if (kstate.IsKeyDown (Keys.S)) {
+                _p1Paddle.moveDown(_graphics);
+            }
+
+            if (kstate.IsKeyDown(Keys.O)) {
+                _p2Paddle.moveUp();
+            } else if (kstate.IsKeyDown(Keys.K)) {
+                _p2Paddle.moveDown(_graphics);
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _p1Paddle.draw(_spriteBatch);
+            _p2Paddle.draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Pong/Paddle.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong
+{
+    internal class Paddle
+    {
+        private Vector2 _position;
+        private float _speed;
+        private Texture2D _texture;
+
+        public Vector2 Position { get { return _position; } }
+        public float Speed { get { return _speed; } }
+        public Texture2D Texture { get { return _texture; }
+            set {
+                _texture ??= value;
+            }
+        }
+    
+
+        public Paddle(Texture2D texture, Vector2 position) {
+            _speed = 2f;
+            _texture = texture;
+            _position = position;
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                this._texture,
+                this._position,
+                null,
+                Color.White,
+                0f, // rotation
+                new Vector2(_texture.Width/2, _texture.Height/2), // draw center
+                new Vector2(1, 2), // scale factors (x,y)
+                SpriteEffects.None,
+                0f);
+
+        }
+
+        public void moveUp()
+        {
+            if(this._position.Y > (this._texture.Height))
+                _position.Y -= _speed;
+        }
+        public void moveDown(GraphicsDeviceManager _graphics)
+        {
+            if (this._position.Y < (_graphics.PreferredBackBufferHeight - this._texture.Height))
+            _position.Y += _speed;
+        }
+
+    }
+}
